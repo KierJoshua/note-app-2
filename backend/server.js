ae59b62c0c29992d823a766cbedc3b17658fd0ad
Kier Joshua <kierjoshuadu1226@gmail.com>
2025-05-05 20:22:58 +0800

    changes at backend

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,26 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
 import path from 'path'
-import { connectDB } from './config/db.js';
-import noteRoutes from './routes/note.routes.js';
+import { connectDB } from './config/db.js'
+import noteRoutes from './routes/note.routes.js'
 
-const app = express();
+const app = express()
 dotenv.config()
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
-const __dirname = path.resolve();
+const __dirname = path.resolve()
 
 app.use(express.json())
 
-app.use(`${import.meta.env.VITE_BACKEND_URL}/notes`, noteRoutes)
-  
+app.use(cors({
+  origin: 'https://note-app-2-6yq1.vercel.app/', // use your actual frontend URL here
+  credentials: true
+}))
 
-app.listen(5000, ()=> {
-    connectDB();
-    console.log(`Server started at http://localhost:${PORT}`)
+app.use('/api/notes', noteRoutes)
+
+app.listen(PORT, () => {
+  connectDB()
+  console.log(`Server started at http://localhost:${PORT}`)
 })
